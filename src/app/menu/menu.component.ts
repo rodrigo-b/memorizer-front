caedbd90d33810d4b5837841eea0f59bbb656316
rodrigo-b <rodrigo-b@black>
2024-03-24 17:33:23 -0300

    initial commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [MatMenuModule,MatToolbarModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
