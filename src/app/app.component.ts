aea8457bd0856aa5c8431a359a5a434a07deda82
rodrigo-b <rodrigo-b@black>
2024-04-04 21:55:48 -0300

    inclusion of footer and home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component'
+import { HomeComponent } from './home/home.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,MenuComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MenuComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'memorizer';
