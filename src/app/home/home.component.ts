c7d1e1a428e1e5113d45f024d83c0d7367abb8f2
rodrigo-b <rodrigo-b@black>
2024-05-02 23:21:45 -0300

    deck inclusion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { MenuComponent } from '../menu/menu.component';
+import { FooterComponent } from '../footer/footer.component';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, FooterComponent, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  decks = [
+    {name: "software archtecture", pendingActivities:"pending 1"},
+    {name: "FrontEnd", pendingActivities:"pending 10"},
+    {name: "backEnd", pendingActivities:"pending 20"},
+    {name: "IA", pendingActivities:"pending 15"},
+  ]
+
 
 }
