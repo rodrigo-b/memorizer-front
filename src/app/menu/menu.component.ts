46445a59444328f32ee9b8dad1801b242389132a
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2024-08-04 17:02:18 -0300

    general background color change and menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { MatMenuModule} from "@angular/material/menu";
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-
 @Component({
   selector: 'app-menu',
   standalone: true,
